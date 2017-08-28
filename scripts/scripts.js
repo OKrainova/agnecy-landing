4e705998596ac53f8136e989e903f01dc2b115c8
Olga Krainova <30428922+OKrainova@users.noreply.github.com>
2017-08-28 22:52:50 +0300

    Add files via upload

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,20 +1,29 @@
 $(document).ready(function () {
     var menu = $('nav.header-menu');
     var patterns = {
-        name: /^[A-Z][a-z]+$/,
+        name: /^[A-ZА-Я][a-zа-я]+$/,
         phone: /^[0-9]{5,15}$/,
         email: /^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i,
         tags: /<[^>]+>/gi
     };
+    
+    $(window).on('scroll', function () {
+        $(this).scrollTop() > 100 ? $(".btn-top").fadeIn() : $(".btn-top").fadeOut();
+    });
+
+    // Кнопка наверх
+    $('.btn-top').on('click', function () {
+        $('html, body').animate({scrollTop: 0}, 700);
+    });
 
     $('.header-menu__toggle').on('click', function (e) {
         menu.toggleClass('open');
     });
 
-    $('*[data-scroll-target]').on('click', function (e) {
+    $('body').on('click', '[data-scroll-target]', function (e) {
         e.preventDefault();
 
-        var target = $(this).data('scroll-target');
+        var target = $(this).data('scroll-target') || $(this).attr('href');
         var offset = $(this).data('scroll-offset') || 0;
         var rate = $(this).data('scroll-rate') || 1000;
         var go = $(target);
@@ -45,6 +54,7 @@ $(document).ready(function () {
         }
     });
 
+    $('.form-control').on()
     function checkInput(value, pattern, patternName) {
         if (value === "")
             return false;
